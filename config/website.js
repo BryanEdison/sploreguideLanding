4358b833accb68d26bd6ff5ec478e24cbbcc3acd
Edison Alulema <ealulem1@binghamton.edu>
2018-07-16 15:24:37 -0400

    hi

---
+++ config/website.js
@@ -0,0 +1,18 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Bella - Gatsby Starter Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Bella', // Alternative Site title for SEO
+  siteUrl: 'https://portfolio-bella.netlify.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
+  siteDescription: 'A bright single-page portfolio starter with big typography & images for Gatsby.',
+
+  siteFBAppID: '123456789', // Facebook App ID
+  userTwitter: '@bella', // Twitter Username
+  ogSiteName: 'bella', // Facebook Site Name
+
+  // Manifest and Progress color
+  themeColor: '#b710a1',
+  backgroundColor: '#350d27',
+};
